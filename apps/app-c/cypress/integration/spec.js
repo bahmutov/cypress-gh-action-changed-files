1f9c1cb08176e96630cef185b6e77ed7970f1ba6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-02-09 21:42:18 -0500

    Make app c next app (#1)

--- apps/app-c/cypress/integration/spec.js
+++ apps/app-c/cypress/integration/spec.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 describe('App C', () => {
   it('loads', () => {
-    cy.visit('index.html')
+    cy.visit('/')
     cy.contains('App C').should('be.visible')
   })
 })
